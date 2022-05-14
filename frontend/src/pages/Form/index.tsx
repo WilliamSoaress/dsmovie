2ef377fa4431900cf5a45dd584c97ac71213e0c8
William Soares <williamsoares058@gmail.com>
2022-05-14 19:47:37 -0300

    Show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -4,7 +4,9 @@ import { useParams } from "react-router-dom";
 function Form() {
   const params = useParams();
 
-  return <FormCard movieId={`${params.movieId}`} />;
+  return ( 
+  <FormCard movieId={`${params.movieId}`} />
+  );
 }
 
 export default Form;
