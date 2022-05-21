0d3aa98f9e3aa7ad09557d7fd8600f1045457c27
William Soares <williamsoares058@gmail.com>
2022-05-21 03:57:53 -0300

    Pequenas melhorias na tela de cadastro de filmes

--- frontend/src/components/RegisterCard/index.tsx
+++ frontend/src/components/RegisterCard/index.tsx
@@ -1,19 +1,16 @@
-
 import axios, { AxiosRequestConfig } from "axios";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/requests";
-import { validateEmail } from "utils/validate";
 import "./styles.css";
 
-console.log("teste")
+console.log("teste");
 
 type Props = {
   movieId: string;
 };
 function RegisterCard({ movieId }: Props) {
-
   const navigate = useNavigate();
 
   const [movie, setMovie] = useState<Movie>();
@@ -25,45 +22,41 @@ function RegisterCard({ movieId }: Props) {
     const image = (event.target as any).image.value;
     const trailer = (event.target as any).trailer.value;
 
-
     const config: AxiosRequestConfig = {
       baseURL: BASE_URL,
-      method: 'POST',
-      url: '/movies/register',
+      method: "POST",
+      url: "/movies/register",
       data: {
         movieId: movieId,
         title: title,
         image: image,
-        trailer: trailer
-      }
-    }
+        trailer: trailer,
+      },
+    };
 
-    axios(config).then(response => {
+    axios(config).then((response) => {
       navigate("/");
-    })
+    });
   };
 
-  
- 
-
   return (
-
     <div className="dsmovie-form-container">
-
       <div className="dsmovie-card-bottom-container">
         <h3>Cadastro de filmes</h3>
         <form className="dsmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="title">Informe o título do filme</label>
-            <input type="text" className="form-control" id="title" />
+            <input type="text" className="form-control" id="title" required={true} />
           </div>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="image">Informe o endereço da imagem do filme</label>
-            <input type="url" className="form-control" id="image" />
+            <input type="url" className="form-control" id="image" required={true} />
           </div>
           <div className="form-group dsmovie-form-group">
-            <label htmlFor="trailer">Informe o endereço do trailer do filme</label>
-            <input type="url" className="form-control" id="trailer" />
+            <label htmlFor="trailer">
+              Informe o endereço do trailer do filme
+            </label>
+            <input type="url" className="form-control" id="trailer" required={true} />
           </div>
 
           <div className="dsmovie-form-btn-container">
@@ -72,8 +65,9 @@ function RegisterCard({ movieId }: Props) {
             </button>
           </div>
         </form>
+        <Link to="/">
           <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-     
+        </Link>
       </div>
     </div>
   );
